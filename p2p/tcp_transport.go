f8892d4c1ae682a9e150085814a7997fd27d75ee
Preston Prince <prestonprince18@gmail.com>
2024-08-23 13:28:53 -0400

    add tcp transport struct

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,20 @@
+package p2p
+
+import (
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
