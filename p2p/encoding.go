560d5614071b0725fbc2d60565d239f733c2108f
Preston Prince <prestonprince18@gmail.com>
2024-09-01 22:37:33 -0400

    message transports

---
+++ p2p/encoding.go
@@ -0,0 +1,32 @@
+package p2p
+
+import (
+	"encoding/gob"
+	"io"
+)
+
+type Decoder interface {
+	Decode(io.Reader, *Message) error
+}
+
+type GOBDecoder struct {
+}
+
+func (dec GOBDecoder) Decode(r io.Reader, msg *Message) error {
+	return gob.NewDecoder(r).Decode(msg)
+}
+
+type DefaultDecoder struct {
+}
+
+func (dec DefaultDecoder) Decode(r io.Reader, msg *Message) error {
+	buf := make([]byte, 1020)
+	n, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	msg.Payload = buf[:n]
+
+	return nil
+}
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -22,23 +22,29 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+type TCPTransportOpts struct {
+	ListenAddr    string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+}
+
 type TCPTransport struct {
-	listenAddress string
-	listener      net.Listener
+	TCPTransportOpts TCPTransportOpts
+	listener         net.Listener
 
 	mu    sync.RWMutex
 	peers map[net.Addr]Peer
 }
 
-func NewTCPTransport(listenAddr string) *TCPTransport {
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
 	return &TCPTransport{
-		listenAddress: listenAddr,
+		TCPTransportOpts: opts,
 	}
 }
 
 func (t *TCPTransport) ListenAndAccept() error {
 	var err error
-	t.listener, err = net.Listen("tcp", t.listenAddress)
+	t.listener, err = net.Listen("tcp", t.TCPTransportOpts.ListenAddr)
 	if err != nil {
 		fmt.Printf("tcp listen error: %s\n", err)
 		return err
@@ -56,14 +62,34 @@ func (t *TCPTransport) startAcceptLoop() {
 		if err != nil {
 			fmt.Printf("TCP accept error: %s\n", err)
 		}
+		fmt.Printf("new incoming connection %+v\n", conn)
 
 		go t.handleConn(conn)
 	}
 
 }
 
+type Temp struct {
+}
+
 func (t *TCPTransport) handleConn(conn net.Conn) {
 	peer := NewTCPPeer(conn, true)
 
-	fmt.Printf("new incoming connection %+v\n", peer)
+	if err := t.TCPTransportOpts.HandshakeFunc(peer); err != nil {
+		conn.Close()
+		fmt.Printf("TCP handshake error: %s\n", err)
+		return
+	}
+
+	// Read loop
+	msg := &Message{}
+	for {
+		if err := t.TCPTransportOpts.Decoder.Decode(conn, msg); err != nil {
+			fmt.Printf("TCP Error: %s\n", err)
+			continue
+		}
+
+		msg.From = conn.RemoteAddr()
+		fmt.Printf("message: %+v\n", msg)
+	}
 }
